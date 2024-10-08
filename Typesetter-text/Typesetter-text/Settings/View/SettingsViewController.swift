7f91042f9d9145839807e98d7bd7a84689422d5d
Karen <karen606>
2024-10-08 15:16:44 +0400

    fixed for ipad

--- Typesetter-text/Typesetter-text/Settings/View/SettingsViewController.swift
+++ Typesetter-text/Typesetter-text/Settings/View/SettingsViewController.swift
@@ -158,6 +158,11 @@ extension SettingsViewController: UIImagePickerControllerDelegate, UINavigationC
                     UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
                 }
             }))
+            if let popoverController = alert.popoverPresentationController {
+                popoverController.sourceView = self.view // Your source view
+                popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+                popoverController.permittedArrowDirections = []
+            }
             present(alert, animated: true, completion: nil)
         }
         
--- Typesetter-text/Typesetter-text/Words/View/WordsViewController.swift
+++ Typesetter-text/Typesetter-text/Words/View/WordsViewController.swift
@@ -176,6 +176,11 @@ extension WordsViewController: DialogViewDelegate {
             self.saveTextAsPDF(content: text)
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        if let popoverController = alert.popoverPresentationController {
+            popoverController.sourceView = self.view // Your source view
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
         present(alert, animated: true, completion: nil)
     }
     
